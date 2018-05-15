6bf35d9ce2e6eff3d36c087393c272682d7df4fd
Joowon Yun <yjw@glosfer.com>
2018-05-15 11:11:26 +0900

    change to difficulty length

--- src/net/Protocol.h
+++ src/net/Protocol.h
@@ -33,14 +33,14 @@ public:
     static const int BLOB_HEX = 144;
     static const int PREHASH_HEX = 128;
     static const int NONCE_HEX = 16;
-    static const int DIFF_HEX = 4;
+    static const int DIFF_HEX = 6;
     static const int RESULT_HEX = 64;
     static const int DIFF_ONE_HEX = 16;
     
     static const int BLOB = 72;
     static const int PREHASH = 64;
     static const int NONCE = 8;
-    static const int DIFF = 2;
+    static const int DIFF = 3;
     static const int RESULT = 32;
     static const int DIFF_ONE = 8;
 
